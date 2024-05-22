04f1455f462e7062efa0a0eec0fb36a1572428a4
Urmatbek Asilbekov <asilbekovurmatbek04@gmail.com>
2024-05-22 11:05:37 +0600

    add all files

--- src/main/java/org/example/dao/PersonDAO.java
+++ src/main/java/org/example/dao/PersonDAO.java
@@ -1,2 +1,33 @@
-package org.example.dao;public class PersonDAO {
+package org.example.dao;
+
+
+import org.example.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+
+
+    private static int COUNT_PEOPLE;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++COUNT_PEOPLE, "Aziret"));
+        people.add(new Person(++COUNT_PEOPLE, "Aknazar"));
+        people.add(new Person(++COUNT_PEOPLE, "Tima"));
+        people.add(new Person(++COUNT_PEOPLE, "Nuraly"));
+    }
+
+    public List<Person> index(){
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
 }
